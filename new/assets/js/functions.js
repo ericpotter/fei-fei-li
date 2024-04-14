5b4e418440692fe0962aeec7d27ee04f16679095
賴宗岳 <26855591lai@gmail.com>
2024-04-15 00:35:42 +0800

    2024/04/15

--- new/assets/js/functions.js
+++ new/assets/js/functions.js
@@ -1,7 +1,16 @@
 $(document).ready(function(){
 
+
+    // 为具有类名为 'side-nav' 的 <li> 元素添加点击事件监听器
+    $('.side-nav li').click(function(){
+        // 在这里添加您想要执行的操作
+        
+    });
+
+
     $("#collapse").on("click", function(){
         $("#right-menu").toggleClass("active");
         $(".fa-bars").toggleClass("fa-bars-staggered");
+        $(".collapse-btn").toggleClass("active");      
     })
 })
