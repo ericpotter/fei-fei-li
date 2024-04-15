7277bcd2d97c1fb134e1420aa1181503eeff66d3
賴宗岳 <26855591lai@gmail.com>
2024-04-15 15:52:47 +0800

    2024/4/15
    side-nav finished

--- new/assets/js/functions.js
+++ new/assets/js/functions.js
@@ -1,10 +1,17 @@
 $(document).ready(function(){
 
 
-    // 为具有类名为 'side-nav' 的 <li> 元素添加点击事件监听器
+
     $('.side-nav li').click(function(){
-        // 在这里添加您想要执行的操作
-        
+        if (!($(this).hasClass('active'))){
+			let $this = $(this),
+				curActive = $this.parent().find('.active'),
+				curPos = $this.parent().children().index(curActive),
+				nextPos = $this.parent().children().index($this),
+				lastItem = $(this).parent().children().length - 1;
+            $('.side-nav').children().removeClass('active');
+            $('.side-nav').children().eq(nextPos).addClass('active');
+		}
     });
 
 
