8c63662bbc5e0041310236f8753565290387b2d8
賴宗岳 <26855591lai@gmail.com>
2024-06-16 00:29:33 +0800

    2024/6/16
    home page finished

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -90,19 +90,39 @@ const translations = {
 		lSideNavDedication: "Dedication",
 		lSideNavMedia: "Media",
 		lSideNavAbout: "About",
+		saying1: "China is a dormant lion. When it stirs, the world will stand in awe.",
+		saying2: "Empower computers with visual intelligence, and create a brighter future for children and the world.",
+		author: "Dr. Fei-Fei Li in <cite title=\"Source Title\" id=\"source-title\">Google Developer Day</cite>",
+		rightHome: "<a href=\"index.html\"><i class=\"fa-solid fa-house nav-options\"></i>Home</a>",
+		rightAbout: "<a href=\"about.html\"><i class=\"fa-solid fa-user nav-options\"></i>About</a>",
+		rightDedictation: "<a href=\"dedication.html\"><i class=\"fa-solid fa-award nav-options\"></i>Dedication</a>",
+		rightMedia: "<a href=\"media.html\"><i class=\"fa-solid fa-photo-film nav-options\"></i>Media</a>",
+		rightContact: "<a href=\"contact.html\"><i class=\"fa-solid fa-envelope nav-options\"></i>Contact</a>",
+		langChange: "<button type=\"button\" class=\"lang-change btn btn-primary\" data-bs-toggle=\"modal\" data-bs-target=\"#language\">Change Language</button>",
+		modalTitle: "Choose a language",
 	},
 	zh: {
 		homeTitle: "揭示<br>AI奇蹟 - <br>李飛飛博士",
 		learnMore: "了解更多 <i class='fa-solid fa-angles-right'></i>",
 		introTitle: "作為領先的女性科學家的開創之旅",
 		introDescription: "李飛飛博士是一位專注於機器學習和人工智慧的領先華裔計算機科學家。她在Google和斯坦福大學擔任重要職務，對計算機視覺和深度學習研究作出了重大貢獻。",
-		headerTitle: "女性科學家",
+		headerTitle: "傑出女性科學家",
 		lSideNavHome: "主頁",
 		lSideNavIntro: "簡介",
 		lSideNavLife: "生平",
 		lSideNavDedication: "貢獻",
 		lSideNavMedia: "媒體",
 		lSideNavAbout: "關於",
+		saying1: "中國是一隻沉睡的獅子，它一旦被驚醒，世界會為之震動。",
+		saying2: "為電腦賦予視覺智能，為兒童和世界創造更美好的未來。",
+		author: "李飛飛博士 <cite title=\"Source Title\" id=\"source-title\">Google開發者日</cite>",
+		rightHome: "<a href=\"index.html\"><i class=\"fa-solid fa-house nav-options\"></i>首頁</a>",
+		rightAbout: "<a href=\"about.html\"><i class=\"fa-solid fa-user nav-options\"></i>關於</a>",
+		rightDedictation: "<a href=\"dedication.html\"><i class=\"fa-solid fa-award nav-options\"></i>貢獻</a>",
+		rightMedia: "<a href=\"media.html\"><i class=\"fa-solid fa-photo-film nav-options\"></i>媒體</a>",
+		rightContact: "<a href=\"contact.html\"><i class=\"fa-solid fa-envelope nav-options\"></i>聯絡資訊</a>",
+		langChange: "<button type=\"button\" class=\"lang-change btn btn-primary\" data-bs-toggle=\"modal\" data-bs-target=\"#language\">改變使用語言</button>",
+		modalTitle: "請選擇語言",
 	}
 };
 
@@ -118,6 +138,16 @@ function setLanguage(lang) {
 	document.getElementById('l-side-nav-dedication').innerHTML = translations[lang].lSideNavDedication;
 	document.getElementById('l-side-nav-media').innerHTML = translations[lang].lSideNavMedia;
 	document.getElementById('l-side-nav-about').innerHTML = translations[lang].lSideNavAbout;
+	document.getElementById('saying-1').innerHTML = translations[lang].saying1;
+	document.getElementById('saying-2').innerHTML = translations[lang].saying2;
+	document.getElementById('author').innerHTML = translations[lang].author;
+	document.getElementById('right-home').innerHTML = translations[lang].rightHome;
+	document.getElementById('right-about').innerHTML = translations[lang].rightAbout;
+	document.getElementById('right-dedication').innerHTML = translations[lang].rightDedictation;
+	document.getElementById('right-media').innerHTML = translations[lang].rightMedia;
+	document.getElementById('right-contact').innerHTML = translations[lang].rightContact;
+	document.getElementById('lang-change').innerHTML = translations[lang].langChange;
+	document.getElementById('modal-title').innerHTML = translations[lang].modalTitle;
 }
 
 // 檢查localStorage中的語言設置
