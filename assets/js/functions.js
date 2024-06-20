37d848027973b7d7dd1ca9698a5b0866ee041a51
Yujuinouobb <161824295+Yujuinouobb@users.noreply.github.com>
2024-06-20 17:48:05 +0800

    0620幫雨馨上傳

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -122,7 +122,22 @@ const translations = {
 		timelineContentThirteen: "She was elected as a member of the National Academy of Engineering. In May, she joined Twitter's board of directors as an independent director, and in October, she was elected as a member of the National Academy of Medicine.",
 		timelineContentFourteen: "She was elected as a member of the American Academy of Arts and Sciences.",
 		timelineContentFifteen: "She was named a 2022 IEEE Fellow.",
-		timelineContentSixteen: "Due to Twitter's acquisition by Elon Musk, Li Feifei stepped down from her role as an independent director at Twitter."
+		timelineContentSixteen: "Due to Twitter's acquisition by Elon Musk, Li Feifei stepped down from her role as an independent director at Twitter.",
+		mediaH1: "Media Library",
+		mediaVideoTitle1:"Fei-Fei Li’s advice to young readers",
+		mediaVideoTitle2:"Fei-Fei Li's Story",
+		mediaVideoTitle3: "Fei-Fei Li's Open Class",
+		aboutDiv1H2:"Motivation for creating the website",
+		aboutDiv1P1Strong:"To promote scientific achievements:",
+		aboutDiv1P1:"To present Fei-Fei Li's research results and contributions to a wider audience in order to increase awareness and understanding of scientific research.",
+		aboutDiv1P2Strong:"Promote gender equality:",
+		aboutDiv1P2:"By showcasing the success stories of women in science, to promote social attention and action on gender equality and reduce gender bias and discrimination.",
+		aboutDiv2H2:"Website Objectives",
+		aboutDiv2P1Strong:"To effectively enable visitors to learn about Fei-Fei Li's life and her contributions.",
+		aboutDiv2P2Strong:"To present Fei-Fei Li's career and achievements",
+		aboutDiv2P2:"In order to encourage young women to pursue their scientific dreams while providing them with role models and inspiration.",
+		aboutDiv3H2:"Project Team Members",
+		aboutDiv3P:"LAI ZONGYUE  -  Leader <br> DU RUILIN <br> WANG YUXIN <br> HUANG YUJUN",
 	},
 	zh: {
 		homeTitle: "揭示<br>AI奇蹟 - <br>李飛飛博士",
@@ -164,11 +179,28 @@ const translations = {
 		timelineContentThirteen: "2020年當選美國國家工程院院士；5月擔任Twitter公司董事會新獨立董事；10月當選美國國家醫學院院士。",
 		timelineContentFourteen: "2021年4月22日，當選為美國文理科學院（藝術與科學院）院士",
 		timelineContentFifteen: "2021年11月當選2022 IEEE Fellow",
-		timelineContentSixteen: "2022年10月27日，因Twitter被埃隆·馬斯克收購，李飛飛不再擔任Twitter公司的獨立董事。"
+		timelineContentSixteen: "2022年10月27日，因Twitter被埃隆·馬斯克收購，李飛飛不再擔任Twitter公司的獨立董事。",
+		mediaH1:'媒體庫',
+		mediaVideoTitle1:'李飛飛對青年讀者的建議',
+		mediaVideoTitle2:"李飛飛的故事",
+		mediaVideoTitle3: "李飛飛的公開課",
+		aboutDiv1H2:"創建網站的動機",
+		aboutDiv1P1Strong:"為了促進科學成果：",
+		aboutDiv1P1:"向更廣泛的觀眾展示費的研究成果和貢獻，以提高人們對科學研究的認識和理解。",
+		aboutDiv1P2Strong:"促進兩性平等：",
+		aboutDiv1P2:"通過展示婦女在科學領域的成功故事，促進社會對性別平等的關注和行動，减少性別偏見和歧視。",
+		aboutDiv2H2:"網站目標",
+		aboutDiv2P1Strong:"有效地讓遊客瞭解李飛飛的生平和貢獻。",
+		aboutDiv2P2Strong:"介紹費的職業生涯和成就",
+		aboutDiv2P2:"為了鼓勵年輕女性追求她們的科學夢想，同時為她們提供榜樣和靈感。",
+		aboutDiv3H2:"項目團隊成員",
+		aboutDiv3P:"赖宗岳  -  组长 <br> 杜睿霖 <br> 王雨馨 <br> 黃伃君",
 	}
 };
 
 function setLanguage(lang) {
+	
+
 	document.getElementById('home-title').innerHTML = translations[lang].homeTitle;
 	document.getElementById('btn-learnMore').innerHTML = translations[lang].learnMore;
 	document.getElementById('intro-title').innerHTML = translations[lang].introTitle;
@@ -187,26 +219,43 @@ function setLanguage(lang) {
 	document.getElementById('right-contact').innerHTML = translations[lang].rightContact;
 	document.getElementById('lang-change').innerHTML = translations[lang].langChange;
 	document.getElementById('modal-title').innerHTML = translations[lang].modalTitle;
-	document.querySelector('.timeline-item').innerHTML = translations[lang].timelineFollowTitle;
-	document.getElementById('timeline-content-one').innerHTML = translations[lang].timelineContentOne;
-	document.getElementById('timeline-childhood').innerHTML = translations[lang].timelineChildhood;
-	document.getElementById('timeline-content-two').innerHTML = translations[lang].timelineContentTwo;
-	document.getElementById('timeline-high-school').innerHTML = translations[lang].timelineHighSchool;
-	document.getElementById('timeline-content-three').innerHTML = translations[lang].timelineContentThree;
-	document.getElementById('timeline-content-four').innerHTML = translations[lang].timelineContentFour;
-	document.getElementById('timeline-content-five').innerHTML = translations[lang].timelineContentFive;
-	document.getElementById('timeline-content-six').innerHTML = translations[lang].timelineContentSix;
-	document.getElementById('timeline-content-seven').innerHTML = translations[lang].timelineContentSeven;
-	document.getElementById('timeline-content-eight').innerHTML = translations[lang].timelineContentEight;
-	document.getElementById('timeline-content-nine').innerHTML = translations[lang].timelineContentNine;
-	document.getElementById('timeline-content-ten').innerHTML = translations[lang].timelineContentTen;
-	document.getElementById('timeline-content-eleven').innerHTML = translations[lang].timelineContentEleven;
-	document.getElementById('timeline-content-twelve').innerHTML = translations[lang].timelineContentTwelve;
-	document.getElementById('timeline-content-thirteen').innerHTML = translations[lang].timelineContentThirteen;
-	document.getElementById('timeline-content-fourteen').innerHTML = translations[lang].timelineContentFourteen;
-	document.getElementById('timeline-content-fifteen').innerHTML = translations[lang].timelineContentFifteen;
-	document.getElementById('timeline-content-fifteen').innerHTML = translations[lang].timelineContentFifteen; 
-	document.getElementById('timeline-content-sixteen').innerHTML = translations[lang].timelineContentSixteen;
+	// document.querySelector('.timeline-item').innerHTML = translations[lang].timelineFollowTitle;
+	// document.getElementById('timeline-content-one').innerHTML = translations[lang].timelineContentOne;
+	// document.getElementById('timeline-childhood').innerHTML = translations[lang].timelineChildhood;
+	// document.getElementById('timeline-content-two').innerHTML = translations[lang].timelineContentTwo;
+	// document.getElementById('timeline-high-school').innerHTML = translations[lang].timelineHighSchool;
+	// document.getElementById('timeline-content-three').innerHTML = translations[lang].timelineContentThree;
+	// document.getElementById('timeline-content-four').innerHTML = translations[lang].timelineContentFour;
+	// document.getElementById('timeline-content-five').innerHTML = translations[lang].timelineContentFive;
+	// document.getElementById('timeline-content-six').innerHTML = translations[lang].timelineContentSix;
+	// document.getElementById('timeline-content-seven').innerHTML = translations[lang].timelineContentSeven;
+	// document.getElementById('timeline-content-eight').innerHTML = translations[lang].timelineContentEight;
+	// document.getElementById('timeline-content-nine').innerHTML = translations[lang].timelineContentNine;
+	// document.getElementById('timeline-content-ten').innerHTML = translations[lang].timelineContentTen;
+	// document.getElementById('timeline-content-eleven').innerHTML = translations[lang].timelineContentEleven;
+	// document.getElementById('timeline-content-twelve').innerHTML = translations[lang].timelineContentTwelve;
+	// document.getElementById('timeline-content-thirteen').innerHTML = translations[lang].timelineContentThirteen;
+	// document.getElementById('timeline-content-fourteen').innerHTML = translations[lang].timelineContentFourteen;
+	// document.getElementById('timeline-content-fifteen').innerHTML = translations[lang].timelineContentFifteen;
+	// document.getElementById('timeline-content-fifteen').innerHTML = translations[lang].timelineContentFifteen; 
+	// document.getElementById('timeline-content-sixteen').innerHTML = translations[lang].timelineContentSixteen;
+	document.getElementById('mediah1').innerHTML = translations[lang].mediaH1;
+	document.getElementById('carousel__caption1').innerHTML = translations[lang].mediaVideoTitle1;
+	document.getElementById('carousel__caption2').innerHTML = translations[lang].mediaVideoTitle2;
+	document.getElementById('carousel__caption3').innerHTML = translations[lang].mediaVideoTitle3;
+	document.getElementById('aboutDiv1H2').innerHTML = translations[lang].aboutDiv1H2;
+	document.getElementById('aboutDiv1P1Strong').innerHTML = translations[lang].aboutDiv1P1Strong;
+	document.getElementById('aboutDiv1P1').innerHTML = translations[lang].aboutDiv1P1;
+	document.getElementById('aboutDiv1P2Strong').innerHTML = translations[lang].aboutDiv1P2Strong;
+	document.getElementById('aboutDiv1P2').innerHTML = translations[lang].aboutDiv1P2;
+	document.getElementById('aboutDiv2H2').innerHTML = translations[lang].aboutDiv2H2;
+	document.getElementById('aboutDiv2P1Strong').innerHTML = translations[lang].aboutDiv2P1Strong;
+	document.getElementById('aboutDiv2P2Strong').innerHTML = translations[lang].aboutDiv2P2Strong;
+	document.getElementById('aboutDiv2P2').innerHTML = translations[lang].aboutDiv2P2;
+	document.getElementById('aboutDiv3H2').innerHTML = translations[lang].aboutDiv3H2;
+	document.getElementById('aboutDiv3P').innerHTML = translations[lang].aboutDiv3P;
+
+
 }
 
 
