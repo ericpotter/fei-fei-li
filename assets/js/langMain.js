e94bba3baf633b7b98c28ddc75339d5690c3f72f
杜睿霖 <pig2709008@gmail.com>
2024-06-21 15:58:07 +0800

    0621

--- assets/js/langMain.js
+++ assets/js/langMain.js
@@ -18,6 +18,21 @@ const translations = {
 		rightContact: "<a href=\"contact.html\"><i class=\"fa-solid fa-envelope nav-options\"></i>Contact</a>",
 		langChange: "<button type=\"button\" class=\"lang-change btn btn-primary\" data-bs-toggle=\"modal\" data-bs-target=\"#language\">Change Language</button>",
 		modalTitle: "Choose a language",
+		mediaH1: "Media Library",
+		mediaVideoTitle1:"Fei-Fei Li’s advice to young readers",
+		mediaVideoTitle2:"Fei-Fei Li's Story",
+		mediaVideoTitle3: "Fei-Fei Li's Open Class",
+		aboutDiv1H2:"Motivation for creating the website",
+		aboutDiv1P1Strong:"To promote scientific achievements:",
+		aboutDiv1P1:"To present Fei-Fei Li's research results and contributions to a wider audience in order to increase awareness and understanding of scientific research.",
+		aboutDiv1P2Strong:"Promote gender equality:",
+		aboutDiv1P2:"By showcasing the success stories of women in science, to promote social attention and action on gender equality and reduce gender bias and discrimination.",
+		aboutDiv2H2:"Website Objectives",
+		aboutDiv2P1Strong:"To effectively enable visitors to learn about Fei-Fei Li's life and her contributions.",
+		aboutDiv2P2Strong:"To present Fei-Fei Li's career and achievements",
+		aboutDiv2P2:"In order to encourage young women to pursue their scientific dreams while providing them with role models and inspiration.",
+		aboutDiv3H2:"Project Team Members",
+		aboutDiv3P:"LAI ZONGYUE  -  Leader <br> DU RUILIN <br> WANG YUXIN <br> HUANG YUJUN",
 	},
 	zh: {
 		homeTitle: "揭示<br>AI奇蹟 - <br>李飛飛博士",
@@ -37,6 +52,21 @@ const translations = {
 		rightContact: "<a href=\"contact.html\"><i class=\"fa-solid fa-envelope nav-options\"></i>聯絡資訊</a>",
 		langChange: "<button type=\"button\" class=\"lang-change btn btn-primary\" data-bs-toggle=\"modal\" data-bs-target=\"#language\">改變使用語言</button>",
 		modalTitle: "請選擇語言",
+		mediaH1:'媒體庫',
+		mediaVideoTitle1:'李飛飛對青年讀者的建議',
+		mediaVideoTitle2:"李飛飛的故事",
+		mediaVideoTitle3: "李飛飛的公開課",
+		aboutDiv1H2:"創建網站的動機",
+		aboutDiv1P1Strong:"為了促進科學成果：",
+		aboutDiv1P1:"向更廣泛的觀眾展示費的研究成果和貢獻，以提高人們對科學研究的認識和理解。",
+		aboutDiv1P2Strong:"促進兩性平等：",
+		aboutDiv1P2:"通過展示婦女在科學領域的成功故事，促進社會對性別平等的關注和行動，减少性別偏見和歧視。",
+		aboutDiv2H2:"網站目標",
+		aboutDiv2P1Strong:"有效地讓遊客瞭解李飛飛的生平和貢獻。",
+		aboutDiv2P2Strong:"介紹費的職業生涯和成就",
+		aboutDiv2P2:"為了鼓勵年輕女性追求她們的科學夢想，同時為她們提供榜樣和靈感。",
+		aboutDiv3H2:"項目團隊成員",
+		aboutDiv3P:"赖宗岳  -  组长 <br> 杜睿霖 <br> 王雨馨 <br> 黃伃君",
 	}
 };
 
@@ -58,6 +88,21 @@ function setLanguage(lang) {
 	document.getElementById('right-contact').innerHTML = translations[lang].rightContact;
 	document.getElementById('lang-change').innerHTML = translations[lang].langChange;
 	document.getElementById('modal-title').innerHTML = translations[lang].modalTitle;
+	document.getElementById('mediah1').innerHTML = translations[lang].mediaH1;
+	document.getElementById('carousel__caption1').innerHTML = translations[lang].mediaVideoTitle1;
+	document.getElementById('carousel__caption2').innerHTML = translations[lang].mediaVideoTitle2;
+	document.getElementById('carousel__caption3').innerHTML = translations[lang].mediaVideoTitle3;
+	document.getElementById('aboutDiv1H2').innerHTML = translations[lang].aboutDiv1H2;
+	document.getElementById('aboutDiv1P1Strong').innerHTML = translations[lang].aboutDiv1P1Strong;
+	document.getElementById('aboutDiv1P1').innerHTML = translations[lang].aboutDiv1P1;
+	document.getElementById('aboutDiv1P2Strong').innerHTML = translations[lang].aboutDiv1P2Strong;
+	document.getElementById('aboutDiv1P2').innerHTML = translations[lang].aboutDiv1P2;
+	document.getElementById('aboutDiv2H2').innerHTML = translations[lang].aboutDiv2H2;
+	document.getElementById('aboutDiv2P1Strong').innerHTML = translations[lang].aboutDiv2P1Strong;
+	document.getElementById('aboutDiv2P2Strong').innerHTML = translations[lang].aboutDiv2P2Strong;
+	document.getElementById('aboutDiv2P2').innerHTML = translations[lang].aboutDiv2P2;
+	document.getElementById('aboutDiv3H2').innerHTML = translations[lang].aboutDiv3H2;
+	document.getElementById('aboutDiv3P').innerHTML = translations[lang].aboutDiv3P;
 }
 
 
